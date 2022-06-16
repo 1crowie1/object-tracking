dd668ddf5e4f169cbe52f43291b01bcddbb17fec
Harrison Crowe-Maxwell <39574251+1crowie1@users.noreply.github.com>
2022-06-16 15:52:53 +1000

    Opening video with opencv2

--- dvd.py
+++ dvd.py
@@ -1,3 +1,20 @@
+import cv2
+import numpy as np
+
 
 def get_collisions(video_path: str, duration: int) -> int:
+    """
+    Return the number of collisions in the video.
+    """
+    cap = cv2.VideoCapture(video_path)
+    ret = True
+    while ret:
+        ret, frame = cap.read()
+        cv2.imshow("DVD Screen", frame)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
     return 0
--- track.py
+++ track.py
@@ -1,6 +1,9 @@
 import dvd
 
 def main() -> None:
+    """
+    Main function.
+    """
     video = 'videos/dvd_screen_saver.mp4'
     seconds = 300
     wall_collisions = dvd.get_collisions(video, seconds)
